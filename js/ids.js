d5e905e2544a358bce484c5edd035436d88d5735
Benkoopa <117493958+Benkoopa@users.noreply.github.com>
2022-11-04 23:42:13 -0500

    Extra Slot El Macho
    Still in early stages, but
    https://gamebanana.com/concepts/2275
    If you need more, here: https://docs.google.com/presentation/d/1GNzWclJfyRNdcdWKvB2II_b-Q10ZvrDqHAFOLqvy644/edit?usp=sharing

--- js/ids.js
+++ js/ids.js
@@ -3,5 +3,6 @@ let registeredID = [
 	1, 2, 3, 4,						// Extra Slot Project (Mighty, Ray, Amy, Kai)
 	5,								// Extra Slot Shadow
 	10, 11, 12,						// Lemons reserved IDs
+	41,							// Benkoopa's Extra Slot El Macho
 	75,                                                     // Just Chris' reserved Extra Slot Extra Life ID
 ];
