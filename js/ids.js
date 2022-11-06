0811406cb8c77eb5ee15b45926b32494a1327e01
Arsenij-Barsukov <100534810+Arsenij-Barsukov@users.noreply.github.com>
2022-11-06 13:14:56 +0300

    Update ids.js

--- js/ids.js
+++ js/ids.js
@@ -1,8 +1,9 @@
 // Here are all the IDs that are currently in use
 let registeredID = [
 	1, 2, 3, 4,						// Extra Slot Project (Mighty, Ray, Amy, Kai)
-	5,								// Extra Slot Shadow
+	5,							// Extra Slot Shadow
 	10, 11, 12,						// Lemons reserved IDs
 	41,							// Benkoopa's Extra Slot El Macho
 	75,                                                     // Just Chris' reserved Extra Slot Extra Life ID
+        13, 14, 15,						// Telejaba's Extra slot Project 2 (Metal, Bunfriend/Encore Bf, Chaos The Impostorඞ)
 ];
