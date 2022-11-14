67d380ae5a4b2153f7792bf5f28fd7bd650b0023
Sam <105883916+SammyGoesHowdy@users.noreply.github.com>
2022-11-14 18:36:39 +0000

    Update ids.js

--- js/ids.js
+++ js/ids.js
@@ -9,4 +9,5 @@ let registeredID = [
 	75,                                                     // Just Chris' reserved Extra Slot Extra Life ID
 	69,							// Wheel Chair
         22, 23, 24,						// Telejaba's Extra slot Project 2 (Metal, Bunfriend/Encore Bf, Chaos The Impostorඞ)
+	131, 							// Extra Slot Sonictuber (By SammyGoesHowdy)
 ];
