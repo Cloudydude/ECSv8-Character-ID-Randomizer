ef625a30ce19bdbc81b3a7664827f6994398bfa4
BLUΞ <88460001+B-L-U-3@users.noreply.github.com>
2022-11-17 18:23:47 -0500

    Remove wheelchair (#9)

--- js/ids.js
+++ js/ids.js
@@ -7,7 +7,6 @@ let registeredID = [
 	17,							// Blue's character
 	41,							// Benkoopa's Extra Slot El Macho
 	8, 75,                                                     // Just Chris' Extra Slot metal knuckles and Extra Slot Extra Life
-	69,							// Wheel Chair
         //Not sure 22, 23, 24,						// Telejaba's Extra slot Project 2 (Metal, Bunfriend/Encore Bf, Chaos The Impostorඞ)
 	131, 							// Extra Slot Sonictuber (By SammyGoesHowdy)
 ];
